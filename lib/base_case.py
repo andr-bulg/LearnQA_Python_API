b7d9238cb8283088d9db11297757c5a00b00dda7
andr-bulg <andr.bulg24@gmail.com>
2024-07-12 14:39:39 +0300

    Ex14: Формирование фреймворка

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,7 +1,9 @@
 import json
-
 from requests import Response
+
+
 class BaseCase:
+
     def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Не найден куки {cookie_name} в последнем ответе"
         return response.cookies[cookie_name]
--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -3,6 +3,7 @@ import pytest
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
+
 class Test_User_Auth(BaseCase):
 
     exclude_params = [("no_cookie"),
@@ -21,7 +22,6 @@ class Test_User_Auth(BaseCase):
         self.token = self.get_header(response_1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response_1, "user_id")
 
-
     def test_user_auth(self):
         response_2 = requests.get(self.url_2, headers={"x-csrf-token": self.token},
                                   cookies={"auth_sid": self.auth_sid})
@@ -30,7 +30,6 @@ class Test_User_Auth(BaseCase):
                                              "user_id",
                                              self.user_id_from_auth_method,
                                              "user_id_from_auth_method не соответствует user_id_from_check_method")
-
 
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth_check(self, condition):
