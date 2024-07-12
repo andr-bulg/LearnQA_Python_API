23f5965182fed952f642e5596a57af4db1bb3867
andr-bulg <andr.bulg24@gmail.com>
2024-07-12 10:19:38 +0300

    Ex12: Тест запроса на метод header

---
+++ lesson_3_homework/test_ex12_check_header_value.py
@@ -0,0 +1,9 @@
+import requests
+
+def test_check_header_value():
+    url = "https://playground.learnqa.ru/api/homework_header"
+    response = requests.get(url)
+    expected_headers = ['Date', 'Content-Type', 'Content-Length', 'Connection', 'Keep-Alive', 'Server', 'x-secret-homework-header', 'Cache-Control', 'Expires']
+    actual_headers = list(response.headers.keys())
+    assert expected_headers == actual_headers, "Фактический набор заголовков отличается от ожидаемого"
+
