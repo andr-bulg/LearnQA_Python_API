aff10e4ae6e731aa5f46bdd16dd2472217d84be6
andr-bulg <andr.bulg24@gmail.com>
2024-07-18 12:49:55 +0300

    Добавлен тест test_create_user_successfully

--- lib/assertions.py
+++ lib/assertions.py
@@ -14,3 +14,19 @@ class Assertions:
         assert name in response_as_dict, f"Ключ {name} отсутствует в JSON ответе"
         assert response_as_dict[name] == expected_value, error_message
 
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Ответ представлен не в JSON формате. Текст ответа: {response.text}"
+
+        assert name in response_as_dict, f"Ключ {name} отсутствует в JSON ответе"
+
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Неожидаемый код ответа! Ожидаемый код: {expected_status_code}. " \
+            f"Фактический код: {response.status_code}."
+
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,8 +1,30 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from datetime import datetime
 
 class Test_User_Register(BaseCase):
+
+    def setup_method(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+        self.url = "https://playground.learnqa.ru/api/user/"
+
+
+    def test_create_user_successfully(self):
+        data = {
+            "username": "learnqa",
+            "firstName": "learnqa",
+            "lastName": "learnqa",
+            "email": self.email,
+            "password": "123"}
+
+        response = requests.post(self.url, data=data)
+        Assertions.assert_code_status(response, 200)
+        print(response.content)
+
 
     def test_create_user_with_existing_email(self):
         email = "vinkotov@example.com"
@@ -13,12 +35,8 @@ class Test_User_Register(BaseCase):
             "email": email,
             "password": "123"}
 
-        url = "https://playground.learnqa.ru/api/user/"
-
-        response = requests.post(url, data=data)
-        assert response.status_code == 400, f"Статус код равен {response.status_code}"
-        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists",\
+        response = requests.post(self.url, data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
                f"content ответа {response.content}"
-        # print(response.status_code)
-        # print(response.content)
 
